5b20ab50ab084228275e315b45be43e256ccd847
Bo Zhang <bo@gradle.com>
2020-11-09 11:39:07 +0800

    disable slow tests

--- .teamcity/DistributedTest/configurations/FunctionalTest.kt
+++ .teamcity/DistributedTest/configurations/FunctionalTest.kt
@@ -23,7 +23,7 @@ class FunctionalTest(
     this.name = name
     this.description = description
     id = AbsoluteId(uuid)
-    val testTaskName = "${testCoverage.testType.name}Test"
+    val testTaskName = "${testCoverage.testType.name}Test -x platform-play:quickTest -x distributions-full:quickTest"
     val quickTest = testCoverage.testType == TestType.quick
     val buildScanTags = listOf("FunctionalTest")
     val buildScanValues = mapOf(
